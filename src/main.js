61cad701c5fed37bf4d96703fa2aa2a4540834ba
milley <milley.gm@gmail.com>
2023-08-02 23:52:28 +0800

    feat: axios component and remove default view

--- src/main.js
+++ src/main.js
@@ -1,11 +1,17 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
+import '@/assets/main.css'
+
+// 测试接口函数
+import { getCategory } from '@/apis/testAPI'
+
+getCategory().then(res => {
+    console.log(res)
+})
 const app = createApp(App)
 
 app.use(createPinia())
