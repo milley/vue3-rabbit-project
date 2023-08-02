e7b17ab71722e97fcdb4b99409338ed6252bdbe4
milley <milley.gm@gmail.com>
2023-08-02 23:28:17 +0800

    element-Plus Theme color customization

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,8 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div>
+    <el-button type="primary">Primary</el-button>
+  </div>
 </template>
