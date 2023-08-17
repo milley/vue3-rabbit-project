6bf2fb76627619dd2da1b777847a41340c0a7ae0
milley <milley.gm@gmail.com>
2023-08-17 23:19:43 +0800

    feat: impl home panel component

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -4,6 +4,7 @@ import HomeBanner from './components/HomeBanner.vue'
 import HomeNew from './components/HomeNew.vue'
 import HomeHot from './components/HomeHot.vue'
 import homeProduct from './components/HomeProduct.vue'
+import homePanel from './components/HomePanel.vue'
 </script>
 
 <template>
@@ -14,4 +15,16 @@ import homeProduct from './components/HomeProduct.vue'
   <HomeNew />
   <HomeHot />
   <homeProduct />
+  <!-- 测试面板组件 -->
+  <homePanel title="新鲜好物" subTitle="新鲜好物 好多商品">
+    <div>
+      我是新鲜好物slot
+    </div>
+  </homePanel>
+
+  <homePanel title="人气推荐" subTitle="人气推荐 好多商品">
+    <div>
+      我是人气推荐slot
+    </div>
+  </homePanel>
 </template>
