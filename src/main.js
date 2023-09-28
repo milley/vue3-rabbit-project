e11373cb80f1c1dead66c467bec5603903bd8e5a
milley <milley.gm@gmail.com>
2023-09-28 22:24:34 +0800

    feat: impl lazy image loading

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import router from "./router";
 
 import "@/styles/common.scss";
 
+import { useIntersectionObserver } from "@vueuse/core";
+
 // 测试接口函数
 // import { getCategory } from '@/apis/testAPI'
 
@@ -18,3 +20,18 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+
+// 定义全局指令
+app.directive("img-lazy", {
+  mounted(el, binding) {
+    // el: 指令绑定的元素 img
+    // binding: binding.value 指令等于号后面绑定的表达式的值 图片url
+    console.log(el, binding.value);
+    useIntersectionObserver(el, ([{ isIntersecting }]) => {
+      console.log(isIntersecting);
+      if (isIntersecting) {
+        el.src = binding.value;
+      }
+    });
+  },
+});
