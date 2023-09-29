86b0014db8d4b622f2f99a84165efba005df9a00
milley <milley.gm@gmail.com>
2023-09-29 14:08:56 +0800

    feat: impl 1st level category-bread nav render

--- src/main.js
+++ src/main.js
@@ -26,9 +26,9 @@ app.directive("img-lazy", {
   mounted(el, binding) {
     // el: 指令绑定的元素 img
     // binding: binding.value 指令等于号后面绑定的表达式的值 图片url
-    console.log(el, binding.value);
+    // console.log(el, binding.value);
     useIntersectionObserver(el, ([{ isIntersecting }]) => {
-      console.log(isIntersecting);
+      // console.log(isIntersecting);
       if (isIntersecting) {
         el.src = binding.value;
       }
--- src/views/Home/components/HomeBanner.vue
+++ src/views/Home/components/HomeBanner.vue
@@ -1,34 +1,30 @@
 <script setup>
-import { ref, onMounted } from 'vue'
-import { getBannerAPI } from '@/apis/home'
+import { ref, onMounted } from "vue";
+import { getBannerAPI } from "@/apis/home";
 
-const bannerList = ref([])
+const bannerList = ref([]);
 const getBanner = async () => {
-    const res = await getBannerAPI()
-    console.log(res)
-    bannerList.value = res.result
-}
+  const res = await getBannerAPI();
+  // console.log(res)
+  bannerList.value = res.result;
+};
 
 onMounted(() => {
-    getBanner()
-})
+  getBanner();
+});
 </script>
-
-
 
 <template>
   <div class="home-banner">
     <el-carousel height="500px">
       <el-carousel-item v-for="item in bannerList" :key="item.id">
-        <img :src="item.imgUrl" alt="">
+        <img :src="item.imgUrl" alt="" />
       </el-carousel-item>
     </el-carousel>
   </div>
 </template>
 
-
-
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .home-banner {
   width: 1240px;
   height: 500px;
