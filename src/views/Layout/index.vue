a2d96d21a742c88c2f4abbe6b2e5e58b2a8f1fe1
milley <milley.gm@gmail.com>
2023-10-15 22:17:16 +0800

    一级分类解决路由缓存问题

--- src/views/Layout/index.vue
+++ src/views/Layout/index.vue
@@ -1,22 +1,24 @@
 <script setup>
-import { onMounted } from 'vue'
-import LayoutNav from './components/LayoutNav.vue'
-import LayoutHeader from './components/LayoutHeader.vue'
-import LayoutFooter from './components/LayoutFooter.vue'
-import LayoutFixed from './components/LayoutFixed.vue'
-import { useCategoryStore } from '@/stores/category'
+import { onMounted } from "vue";
+import LayoutNav from "./components/LayoutNav.vue";
+import LayoutHeader from "./components/LayoutHeader.vue";
+import LayoutFooter from "./components/LayoutFooter.vue";
+import LayoutFixed from "./components/LayoutFixed.vue";
+import { useCategoryStore } from "@/stores/category";
 
-const categoryStore = useCategoryStore()
+const categoryStore = useCategoryStore();
 
 onMounted(() => {
-    categoryStore.getCategory()
-})
-</script> 
+  categoryStore.getCategory();
+});
+</script>
 
 <template>
-    <LayoutFixed />
-    <LayoutNav />
-    <LayoutHeader />
-    <RouterView />
-    <LayoutFooter />
+  <LayoutFixed />
+  <LayoutNav />
+  <LayoutHeader />
+  <!--强制销毁重建，添加key简单粗暴-->
+  <!--<RouterView :key="$route.fullPath" />-->
+  <RouterView />
+  <LayoutFooter />
 </template>
