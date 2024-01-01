61343bf901d580ec1ddc15da9f08d8519c75949c
milley <milley.gm@gmail.com>
2024-01-01 22:07:51 +0800

    详情页-lazyPlugin组件注册

---
+++ src/directives/index.js
@@ -0,0 +1,21 @@
+import { useIntersectionObserver } from "@vueuse/core";
+
+// 定义全局指令
+export const lazyPlugin = {
+  install(app) {
+    app.directive("img-lazy", {
+      mounted(el, binding) {
+        // el: 指令绑定的元素 img
+        // binding: binding.value 指令等于号后面绑定的表达式的值 图片url
+        // console.log(el, binding.value);
+        const { stop } = useIntersectionObserver(el, ([{ isIntersecting }]) => {
+          // console.log(isIntersecting);
+          if (isIntersecting) {
+            el.src = binding.value;
+            stop();
+          }
+        });
+      },
+    });
+  },
+};
--- src/main.js
+++ src/main.js
@@ -5,8 +5,7 @@ import App from "./App.vue";
 import router from "./router";
 
 import "@/styles/common.scss";
-
-import { useIntersectionObserver } from "@vueuse/core";
+import { lazyPlugin } from "@/directives";
 import { componentPlugin } from "@/components";
 
 // 测试接口函数
@@ -19,21 +18,7 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(lazyPlugin);
 app.use(componentPlugin);
 
 app.mount("#app");
-
-// 定义全局指令
-app.directive("img-lazy", {
-  mounted(el, binding) {
-    // el: 指令绑定的元素 img
-    // binding: binding.value 指令等于号后面绑定的表达式的值 图片url
-    // console.log(el, binding.value);
-    useIntersectionObserver(el, ([{ isIntersecting }]) => {
-      // console.log(isIntersecting);
-      if (isIntersecting) {
-        el.src = binding.value;
-      }
-    });
-  },
-});
