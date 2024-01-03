4acc67ee4fe3e9a82c0460903928185a6eee70be
milley <milley.gm@gmail.com>
2024-01-03 20:08:52 +0800

    登录-表单校验-自定义校验规则

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -3,6 +3,7 @@ import { ref } from "vue";
 const form = ref({
   account: "",
   password: "",
+  agree: true,
 });
 
 const rules = {
@@ -10,6 +11,18 @@ const rules = {
   password: [
     { required: true, message: "密码不能为空", trigger: "blur" },
     { min: 6, max: 14, message: "密码必须介于6-14长度之间", trigger: "blur" },
+  ],
+  agree: [
+    {
+      validator: (rule, value, callback) => {
+        console.log(value);
+        if (value) {
+          callback();
+        } else {
+          callback(new Error("请勾选协议"));
+        }
+      },
+    },
   ],
 };
 </script>
@@ -48,8 +61,8 @@ const rules = {
               <el-form-item prop="password" label="密码">
                 <el-input v-model="form.password" />
               </el-form-item>
-              <el-form-item label-width="22px">
-                <el-checkbox size="large">
+              <el-form-item prop="agree" label-width="22px">
+                <el-checkbox size="large" v-model="form.agree">
                   我已同意隐私条款和服务条款
                 </el-checkbox>
               </el-form-item>
