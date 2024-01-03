00e84c68d3ee9b6e97d212db5cce73e9cfcc7a05
milley <milley.gm@gmail.com>
2024-01-03 19:58:39 +0800

    登录-表单校验实现

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,4 +1,18 @@
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+const form = ref({
+  account: "",
+  password: "",
+});
+
+const rules = {
+  account: [{ required: true, message: "用户名不能为空", trigger: "blur" }],
+  password: [
+    { required: true, message: "密码不能为空", trigger: "blur" },
+    { min: 6, max: 14, message: "密码必须介于6-14长度之间", trigger: "blur" },
+  ],
+};
+</script>
 
 <template>
   <div>
@@ -21,12 +35,18 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form
+              :model="form"
+              :rules="rules"
+              label-position="right"
+              label-width="60px"
+              status-icon
+            >
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="form.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input v-model="form.password" />
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
