6f55e6fdd0ea17b4196ec5695c9ff977c52fe46a
milley <milley.gm@gmail.com>
2024-01-14 22:42:00 +0800

    结算-生成订单功能实现

--- src/apis/checkout.js
+++ src/apis/checkout.js
@@ -5,3 +5,11 @@ export const getCheckInfoAPI = () => {
     url: "/member/order/pre",
   });
 };
+
+export const createOrderAPI = (data) => {
+  return httpInstance({
+    url: "/member/order",
+    method: "POST",
+    data,
+  });
+};
