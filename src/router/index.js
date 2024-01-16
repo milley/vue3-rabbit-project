26ee8659a5fbebc71fba4ab5d820b47ba8a05cf3
milley <milley.gm@gmail.com>
2024-01-16 22:51:11 +0800

    会员中心-整体功能梳理和路由配置

--- src/router/index.js
+++ src/router/index.js
@@ -9,6 +9,9 @@ import CartList from "@/views/CartList/index.vue";
 import Checkout from "@/views/Checkout/index.vue";
 import Pay from "@/views/Pay/index.vue";
 import PayBack from "@/views/Pay/PayBack.vue";
+import Member from "@/views/Member/index.vue";
+import UserInfo from "@/views/Member/components/UserInfo.vue";
+import UserOrder from "@/views/Member/components/UserOrder.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -22,32 +25,46 @@ const router = createRouter({
           component: Home,
         },
         {
-          path: "/category/:id",
+          path: "category/:id",
           component: Category,
         },
         {
-          path: "/category/sub/:id",
+          path: "category/sub/:id",
           component: SubCategory,
         },
         {
-          path: "/detail/:id",
+          path: "detail/:id",
           component: Detail,
         },
         {
-          path: "/cartlist",
+          path: "cartlist",
           component: CartList,
         },
         {
-          path: "/checkout",
+          path: "checkout",
           component: Checkout,
         },
         {
-          path: "/pay",
+          path: "pay",
           component: Pay,
         },
         {
           path: "paycallback",
           component: PayBack,
+        },
+        {
+          path: "member",
+          component: Member,
+          children: [
+            {
+              path: "user",
+              component: UserInfo,
+            },
+            {
+              path: "order",
+              component: UserOrder,
+            },
+          ],
         },
       ],
     },
